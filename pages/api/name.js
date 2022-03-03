903ceff42fed08761e965cb9a372d034bec487fb
Beaumont Yun <b.yun@hotmail.com>
2022-03-03 11:32:22 +0800

    Updates

--- pages/api/name.js
+++ pages/api/name.js
@@ -1,21 +1,15 @@
 import prisma from "../../lib/prisma";
 
-export default async function assetHandler(req, res) {
-    const { method } = req;
-
-    switch (method) {
-        case "GET":
-            try {
-                const data = await prisma.product.findMany({});
-                res.status(200).json({data});
-            } catch (e) {
-                console.log("Request error", e);
-                res.status(500).json({ error: "Error fetching posts" });
-            }
-            break;
-        default:
-            res.setHeader("Allow", ["GET"]);
-            res.status(405).end(`Method ${method} Not Allowed`);
-            break;
+export default async function handler(req, res) {
+    if (req.method === 'GET') {
+      try {
+        const data = await prisma.product.findMany({});
+        return res.status(200).json({ data });
+      } catch (err) {
+        console.error(err);
+        return res.status(500).json({ msg: 'Something went wrong' });
+      }
+    } else {
+      return res.status(405).json({ msg: 'Method not allowed' });
     }
-}
+  }
